04248006d8a85f2e6eb54fbc24c26d9046b1ddf1
sankalpa <sankalpaacharya6609@gmail.com>
2022-02-19 23:14:20 +0545

    made new home page and modified css of /video

--- sajilopadhai/settings.py
+++ sajilopadhai/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!90_s$_2afrgmzqv2@=gfo8+!q9mua%s_+py8-aa%j4$op#zcs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
